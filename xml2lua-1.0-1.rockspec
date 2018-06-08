1fa796d6d661b5b9d760a9ebd696167212daf274
LunaCI <lunaci@luadist.org>
2018-06-08 03:23:47 +0000

    Update to version 1.0-1

--- xml2lua-1.0-1.rockspec
+++ xml2lua-1.0-1.rockspec
@@ -1,16 +1,16 @@
 -- This file was automatically generated for the LuaDist project.
 
 package = "xml2lua"
-version = "1.0-0"
+version = "1.0-1"
 -- LuaDist source
 source = {
-  tag = "1.0-0",
+  tag = "1.0-1",
   url = "git://github.com/LuaDist-testing/xml2lua.git"
 }
 -- Original source
 -- source = {
 --    url = "git://github.com/manoelcampos/xml2lua",
---    tag = "v1.0-0"
+--    tag = "v1.0-1"
 -- }
 description = {
    summary = "An XML Parser written entirely in Lua 5.",
--- xml2lua.lua
+++ xml2lua.lua
@@ -248,7 +248,7 @@ XmlParser = {
 --@param handler Handler module to be used to convert the XML string
 --to another formats. See the available handlers at the handler directory.
 -- Usually you get an instance to a handler module using, for instance:
--- local handler = require("xml2lua/handler/tree").
+-- local handler = require("xmlhandler/tree").
 --@return a XmlParser object used to parse the XML
 --@see XmlParser
 function parser(handler)     
@@ -369,7 +369,7 @@ function XmlParser:parse(str, parseAttributes)
             end
         elseif string.sub(tagstr,1,8) == "!DOCTYPE" then
             -- Parse DTD
-            match,endmatch,attrs = self:_parseDTD(string,pos)
+            match,endmatch,attrs = self:_parseDTD(str,pos)
             if not match then 
                 self:_err(self._errstr.dtdErr,pos)
             end 
